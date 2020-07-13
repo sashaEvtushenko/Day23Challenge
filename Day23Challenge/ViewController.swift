e6b3b58cdd318a3ef8305d212ab0fb2c80352d94
Sasha Evtushenko <sasha.evtushenko.md@gmail.com>
2020-07-13 22:33:48 +0300

    Initial Commit

---
+++ Day23Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day23Challenge
+//
+//  Created by Sasha Evtushenko on 7/13/20.
+//  Copyright © 2020 Sasha Evtushenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
